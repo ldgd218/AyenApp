a53892a58a0a5a9d96f683f11a5f308b38c16f87
monodeepdas112 <monodeepdas1215@gmail.com>
2017-08-14 01:34:24 +0530

    Initial commit - Splash Screen added

---
+++ app/src/main/java/com/ayen/ayenapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ayen.ayenapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by monodeepdas112 on 14/8/17.
+ */
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash_screen);
+    }
+}
